3607dc9c493d9f0a0f355599c010317a9b3aadfe
Ольга Горбачева <olik.ol@icloud.com>
2022-04-23 16:49:06 +0300

    Initial Commit

---
+++ MVVM_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_Practice
+//
+//  Created by Ольга Горбачева on 23.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
